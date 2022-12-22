27da552ea0f4590af425300548ae1fe289378197
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-12-22 12:26:01 -0600

    Created the game skeleton to dislay players, welcome, and game rules

--- ai.py
+++ ai.py
@@ -4,9 +4,9 @@ class Ai(Player):
     def __init__(self,name):
         super().__init__(name)
         # THESE ARE THE VALUES IT GETS FROM INHARETANCE 
-        self.list_of_gestures = ['Rock','Paper','Scissors','Lizard','Spock']
-        self.chosen_gesture = ""
-        self.wins = 0
-        self.name = name
+        # self.list_of_gestures = ['Rock','Paper','Scissors','Lizard','Spock']
+        # self.chosen_gesture = ""
+        # self.wins = 0
+        # self.name = name
 
 
