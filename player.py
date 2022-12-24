9025e0c226135067b62868dc31570cb7454cdf6a
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2022-12-24 17:22:45 -0600

    Inserted the player input to begin the function of using the player input for their chosen gesture

--- player.py
+++ player.py
@@ -2,10 +2,16 @@ class Player:
 
     def __init__(self, name, win_lose_pairs):
         self.list_of_gestures = ['Rock','Paper','Scissors','Lizard','Spock']
-        # "" using quotes as a placeholder, it will hold items from out list eventually
         self.chosen_gesture = ""
         self.wins = 0
         self.name = name
         self.win_lose_pairs = [('Scissors', 'Paper'), ('Paper', 'Rock'), ('Rock','Lizard'), ('Lizard', 'Spock'), ('Spock', 'Scissors'), ('Lizard', 'Paper'), ('Paper','Spock'),('Spock', 'Rock'),('Rock', 'Scissors')]
 
-        
+    def list_index(self): 
+        self.list_index = 0,1,2,3,4
+        pass
+
+    print('Please choose one of the options below:')
+    print('-> 0 for Rock\n -> 1 for Paper\n -> 2 for Scissors\n -> 3 for Lizard\n -> 4 for Spock')
+
+    player_input = int(input('Your choice (0-4):'))
